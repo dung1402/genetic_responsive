9c12304339e8f6e70493ad7594e74bf505c81805
dung1402 <thuydungduongthi04@gmail.com>
2021-08-12 17:25:38 +0700

    add

---
+++ BTZone/Assets/js/main.js
@@ -0,0 +1,15 @@
+
+
+$( document ).ready(function() {
+    // function slider
+    $('.slider').slick({
+        infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: false,
+    //   autoplay: true,
+      autoplaySpeed: 2000
+    });
+});
+
